651753b0388ebff53b9e68fab562f8069da10921
Khalid Almalki <khalid3obaid@gmail.com>
2018-09-29 04:24:55 +0300

    initial css file and folder added

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,37 @@
 	$(document).ready(function(){
 
+
+	//five-point gpa system
+
+		$('.five-gpa').click(function(){
+			$('option[data-option=1]').val('5.00');
+			$('option[data-option=2]').val('4.75');
+			$('option[data-option=3]').val('4.50');
+			$('option[data-option=4]').val('4.00');
+			$('option[data-option=5]').val('3.50');
+			$('option[data-option=6]').val('3.00');
+			$('option[data-option=7]').val('2.50');
+			$('option[data-option=8]').val('2.00');
+			$('option[data-option=9]').val('1.00');
+		});
+
+
+		//four-point gpa system
+
+			$('.four-gpa').click(function(){
+				$('option[data-option=1]').val('4.00');
+				$('option[data-option=2]').val('3.75');
+				$('option[data-option=3]').val('3.50');
+				$('option[data-option=4]').val('3.00');
+				$('option[data-option=5]').val('2.50');
+				$('option[data-option=6]').val('2.00');
+				$('option[data-option=7]').val('1.50');
+				$('option[data-option=8]').val('1.00');
+				$('option[data-option=9]').val('0.00');
+			});
+
+
+//calculate gpa on button click
 		$('.calc').click(function(){
 			var subject_count = $('.subject').length;
 			var total_hours = 0.0;
@@ -16,9 +48,10 @@
 			$('.gpa-grade').html(gpa);
 		});
 
+//add subject on button click
 		$('.add_subject').click(function(){
-			$('.subject:last').clone().insertAfter('.subject:last');	
-			var subject_count = $('.subject').length;	
+			$('.subject:last').clone().insertAfter('.subject:last');
+			var subject_count = $('.subject').length;
 			$('.subject:last').attr('id', 'subject'+subject_count);
 			$('.subject:last .subject-number').html(subject_count);
 			$('.subject:last .grade'+(subject_count-1)).removeClass().addClass('grade'+subject_count);
